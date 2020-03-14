cd7c26efd5f760a3ecf15dd584b1209d2a0ae706
marcio22bk <marcio22bk@hotmail.com>
2020-03-14 12:21:52 -0300

    att arquivos

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import {baseUrl} from './modulos/config.js'
 
 const apiUrl = baseUrl + '/genero/';
+const apiUrlLivro = baseUrl + '/busca/livro/genero/';
 console.log(apiUrl);
 
 const listar_genero = Vue.extend({
@@ -27,10 +28,43 @@ const listar_genero = Vue.extend({
     }
 });
 
+const listar_livros = Vue.extend({
+    template: '#listar_livros',
+    data: function () {
+        return {
+            livros: [],
+            palavraChave: ''
+        };
+    },
+    computed: {
+        listaLivros: function () {
+            return this.livros.filter((livro) => {
+                return livro.nome.indexOf(this.palavraChave) > -1
+            });
+        }
+    },
+    mounted: function () {
+        fetch(apiUrlLivro).then((response) => {
+            return response.json().then((json) => {
+                this.livros = json;
+            })
+        });
+    }
+});
+
 const router = new VueRouter({
     routes: [
         { path: '/', component: listar_genero },
+    ] 
+});
+
+
+const routerLivro = new VueRouter({
+    routes: [
+        { path: '/livros', component: listar_livros},
     ]
+    
 });
 
 new Vue({router}).$mount('#app');
+new Vue({routerLivro}).$mount('#listaLivros');
