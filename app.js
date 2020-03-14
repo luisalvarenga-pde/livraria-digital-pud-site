9baefe97184c710a6c0d5912756db66df0539d3d
marcio22bk <marcio22bk@hotmail.com>
2020-03-14 11:30:43 -0300

    criando e alterando arquivos

---
+++ app.js
@@ -0,0 +1,35 @@
+import {baseUrl} from 'modulos/config.js'
+
+const apiUrl = baseUrl + '/genero/';
+
+const listar_genero = Vue.extend({
+    template: '#listar-genero',
+    data: function () {
+        return {
+            generos: [],
+            palavraChave: ''
+        };
+    },
+    computed: {
+        listaGeneros: function () {
+            return this.generos.filter((genero) => {
+                return genero.nome.indexOf(this.palavraChave) > -1
+            });
+        }
+    },
+    mounted: function () {
+        fetch(apiUrl).then((response) => {
+            return response.json().then((json) => {
+                this.generos = json;
+            })
+        });
+    }
+});
+
+const router = new VueRouter({
+    routes: [
+        { path: '/', component: listar_genero },
+    ]
+});
+
+new Vue({router}).$mount('#app');
